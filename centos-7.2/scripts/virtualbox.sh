4fce5c2e92bf665cd8b24a11852b4121ec8f7780
Sergio Francisco <sergiofsfilho@gmail.com>
2016-11-22 00:48:33 -0200

    CentOS 7.2 template is almost working

--- centos-7.2/scripts/virtualbox.sh
+++ centos-7.2/scripts/virtualbox.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-# Script to install VirtualBox Guest Additions after
-# installation of the operating system.
+# Script to install VirtualBox Guest Additions after installation of the operating system.
 
 # Examples
 # - https://github.com/hnakamur/my-packer-template-files/blob/master/centos6.5/scripts/virtualbox.sh
 # - https://github.com/boxcutter/centos/blob/master/script/virtualbox.sh
+# - https://github.com/TFDuesing/packer-templates/blob/master/Fedora-23/scripts/10virtualbox.sh
 
-echo "==> Installing VirtualBox Guest Additions through script"
-
-cd /tmp
-mkdir /mnt/cdrom
-mount /home/vagrant/vbox-guest-additions.iso /mnt/cdrom
-sh /mnt/cdrom/VBoxLinuxAdditions.run --nox11
-
-	if [ $? == "0" ]; then
+if [ $PACKER_BUILDER_TYPE = "virtualbox-iso" ]; then
+	yum install -y bzip2 dkms kernel-devel
+	echo "==> Installing VirtualBox Guest Additions through script"
+	mount -o loop /root/vbox-guest-additions.iso /mnt
+	sh /mnt/VBoxLinuxAdditions.run --nox11
+	
+	if [[ $? -eq 0 ]]; then
 		echo "==> VirtualBox Guest Additions successfully installed."
 	else
-		echo "==> Errors were found during VirtualBox Guest Additions installation. Please check and try again."
+		echo "==> Errors were found during VirtualBox Guest Additions installation. Please, verify what happened and try again."
 	fi
-
-umount /mnt/cdrom
-rm -rf /mnt/cdrom
-rm -f /home/vagrant/vbox-guest-additions.iso
+	
+	umount /mnt/
+	rm -f /root/vbox-guest-additions.iso
+fi
